40571c435c8f83e866951ad010d7e3d1d59a5ed1
javguagar <javguagar@alu.edu.gva.es>
2024-09-12 11:30:12 +0200

    a

--- E1/src/Main.java
+++ E1/src/Main.java
@@ -1,5 +1,18 @@
+class Persona implements Alumno{}
+interface Alumno    {}
+
 public class Main {
+
+    static void imprimirJerarquiaDeClasses(Class<?> c) {
+        System.out.print(c.getName() + (c.getSuperclass() != null ? "  >  " : "\n"));
+        if (c.getSuperclass() == null) return;
+        imprimirJerarquiaDeClasses(c.getSuperclass());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Adios");
+
+            Alumno pastorBelga = new Alumno(){};
+
+        imprimirJerarquiaDeClasses(pastorBelga.getClass());
     }
 }
